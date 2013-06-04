4924fe80fc58dcc7d0e41cb0c7eac7d501bc4e2e
Francisco Veloz <fveloz@me.com>
2013-06-04 09:58:30 -0400

    Created User Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130521145605) do
+ActiveRecord::Schema.define(version: 20130604135109) do
 
   create_table "stories", force: true do |t|
     t.string   "title"
     t.string   "link"
     t.integer  "upvotes"
     t.string   "category"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email_address"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
